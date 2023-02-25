6e0d7ab29e63a6b794efd408ae1a27469a3a911d
Pedrojimvi <pedrojv1030@gmail.com>
2023-02-25 14:41:33 +0100

    Added some classes

---
+++ chat/src/main/java/com/user/ThreadUser.java
@@ -0,0 +1,40 @@
+package com.user;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ThreadUser implements Runnable {
+
+    private BufferedReader bfRd;
+
+    public ThreadUser(Socket socket) throws IOException {
+        this.bfRd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                String message = bfRd.readLine();
+                System.out.println(message);
+            }
+        }
+        catch (SocketException e) {
+            System.out.println("Has salido del chat");
+        }
+        catch (IOException exception) {
+            System.out.println(exception);
+        }
+        finally {
+            try {
+                bfRd.close();
+            }
+            catch (Exception exception) {
+                System.out.println(exception);
+            }
+        }
+    }
+}
---
+++ chat/src/main/java/com/user/User.java
@@ -0,0 +1,38 @@
+package com.user;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class User {
+    public static void main(String[] args) {
+        String nom;
+        String res;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu nombre: ");
+        nom = sc.nextLine();
+        res = nom;
+
+        try (Socket socket = new Socket("localhost", 5000)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            ThreadUser threadUser = new ThreadUser(socket);
+            new Thread(threadUser).start();
+
+            out.println(res + ": se ha conectado al chat");
+            do {
+                String message = (nom + " : ");
+                res = sc.nextLine();
+                if (res.equals("cerrar sesión")) {
+                    out.println("cerrar sesión");
+                    break;
+                }
+                out.println(message + res);
+            }
+            while (!res.equals("cerrar sesión"));
+        }
+        catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
